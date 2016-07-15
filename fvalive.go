6f2f4c38cf8a32dcc8b79037b097eafd8966110d
dbasak <dbasak@bol.com>
2016-07-15 14:09:12 +0200

    Std out in the app
    Change-Id: I41df6c8da57edb7228db009139c118a08534c0b4

--- fvalive.go
+++ fvalive.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"os"
-	"os/exec"
 	"bufio"
 	"fmt"
+	"os"
+	"os/exec"
 )
 
 func main() {
@@ -12,17 +12,28 @@ func main() {
 }
 
 func start(cmd_line string) {
+
+	fmt.Println("Starting app..")
+
 	cmd := exec.Command("sh", "-c", cmd_line)
 	stdout, _ := cmd.StdoutPipe()
+	stderr, _ := cmd.StderrPipe()
+
 	in := bufio.NewScanner(stdout)
+	err_stream := bufio.NewScanner(stderr)
+
+	go collect(in)
+	go collect(err_stream)
 
 	err_on_run := cmd.Run()
 
+	if err_on_run != nil {
+		start(cmd_line)
+	}
+}
+
+func collect(in *bufio.Scanner) {
 	for in.Scan() {
 		fmt.Println(in.Text())
-	}
-
-	if err_on_run != nil{
-		start(cmd_line)
 	}
 }
